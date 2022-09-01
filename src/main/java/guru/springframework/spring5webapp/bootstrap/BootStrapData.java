75fa7223fd3256dfe3116449a82080f9869ac93c
Book-Shelf <arthuradd@yahoo.com>
2022-09-01 17:47:10 +0200

    Basic information added

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author malcolm = new Author("Malcolm", "Rolski");
+        Book dad = new Book("Dad all Dead", "123321");
+        Publisher cant = new Publisher();
+        cant.setName("Cantin");
+        cant.setAddressLine("Lolita");
+        cant.setCity("krakow");
+        cant.setState("Malopolska");
+        cant.setZip("3444");
+
+        publisherRepository.save(cant);
+
+        malcolm.getBooks().add(dad);
+        dad.getAuthors().add(malcolm);
+
+        authorRepository.save(malcolm);
+        bookRepository.save(dad);
+
+        Author rod = new Author("Rodrigo", "Marines");
+        Book gog = new Book("Go Oranges Gradle", "753");
+
+        rod.getBooks().add(gog);
+        gog.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(gog);
+
+        System.out.println("Bootstrap started!");
+        System.out.println("Numbers of books: " + bookRepository.count());
+    }
+}
